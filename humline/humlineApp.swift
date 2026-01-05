5c60be9c6f97614168dfbcacec38c458269eca8c
Hassan Qasemi <hassanak813@outlook.com>
2026-01-05 04:05:25 +0330

    batman

---
+++ humline/humlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  humlineApp.swift
+//  humline
+//
+//  Created by Hassan Qasemi on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct humlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
